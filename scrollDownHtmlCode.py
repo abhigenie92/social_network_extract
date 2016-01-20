46566a6de49459150247c0923b3cd858fc9716cd
Anu$hk@ <Anu$hk@>
2016-01-20 12:52:03 +0530

    Updated method

--- scrollDownHtmlCode.py
+++ scrollDownHtmlCode.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
@@ -24,7 +23,7 @@ def return_html_code(url):
 	driver.maximize_window()
 	driver.get(url)
 	# initial wait for the tweets to load
-	wait = WebDriverWait(driver, 10)
+	wait = WebDriverWait(driver, 30)
 	wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "li[data-item-id]")))
 	# scroll down to the last tweet until there is no more tweets loaded
 	print "Scrolling to retrieve all tweets..."
@@ -40,12 +39,7 @@ def return_html_code(url):
 	    try:
 	        wait.until(wait_for_more_than_n_elements_to_be_present((By.CSS_SELECTOR, "li[data-item-id]"), number_of_tweets))
 	    except TimeoutException:
-	    	try:
-	    		print"\nLoading taking too long. Waiting to load.."
-	    		wait = WebDriverWait(driver, 10)
-	    		wait.until(wait_for_more_than_n_elements_to_be_present((By.CSS_SELECTOR, "li[data-item-id]"), number_of_tweets))
-	    	except TimeoutException:
-	       		 break
+	       	break
 	print number_of_tweets       
 	html_full_source=driver.page_source
 	driver.close()
