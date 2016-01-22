e4c783949bbde2827275c10230dc52c731ae5de8
Ubuntu <ubuntu@ip-172-31-38-123.us-west-2.compute.internal>
2016-01-22 08:12:44 +0000

    Headless model code added

--- scrollDownHtmlCode.py
+++ scrollDownHtmlCode.py
@@ -5,7 +5,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import urllib,requests,unidecode,lxml,pdb
-
+from pyvirtualdisplay import Display
+from xvfbwrapper import Xvfb
 class wait_for_more_than_n_elements_to_be_present(object):
     def __init__(self, locator, count):
         self.locator = locator
@@ -19,11 +20,13 @@ class wait_for_more_than_n_elements_to_be_present(object):
             return False
 
 def return_html_code(url):
+	vdisplay =Xvfb()
+	vdisplay.start()
 	driver = webdriver.Firefox()
 	driver.maximize_window()
 	driver.get(url)
 	# initial wait for the tweets to load
-	wait = WebDriverWait(driver, 30)
+	wait = WebDriverWait(driver, 240)
 	wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "li[data-item-id]")))
 	# scroll down to the last tweet until there is no more tweets loaded
 	print "Scrolling to retrieve all tweets..."
@@ -35,13 +38,17 @@ def return_html_code(url):
 	    if number_of_tweets > num_rightnow[z]:
 	    	print num_rightnow[z],
 	    	z=z+1
-	    driver.execute_script("arguments[0].scrollIntoView();", tweets[-1])
+	    driver.execute_script("arguments[0].scrollIntoView(true);", tweets[-1])
 	    try:
 	        wait.until(wait_for_more_than_n_elements_to_be_present((By.CSS_SELECTOR, "li[data-item-id]"), number_of_tweets))
 	    except TimeoutException:
-	       	break
+		try:
+			wait.until(wait_for_more_than_n_elements_to_be_present((By.CSS_SELECTOR, "li[data-item-id]"), number_of_tweets))
+		except TimeoutException:
+       			break
 	print number_of_tweets       
 	html_full_source=driver.page_source
 	driver.close()
+	vdisplay.stop()
 	return html_full_source
 	
