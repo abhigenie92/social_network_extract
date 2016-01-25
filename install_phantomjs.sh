6bebec3c8b96472eb64cee5e858785151a692b41
Abhishek Bhatia <abhigenie92@gmail.com>
2016-01-25 21:23:43 +0530

    Update install_phantomjs.sh

--- install_phantomjs.sh
+++ install_phantomjs.sh
@@ -25,7 +25,7 @@ sudo apt-get install libfreetype6 libfreetype6-dev -y
 sudo apt-get install libfontconfig1 libfontconfig1-dev -y
 
 cd ~
-wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 sudo tar xvjf $PHANTOM_JS.tar.bz2
 
 sudo mv $PHANTOM_JS /usr/local/share
